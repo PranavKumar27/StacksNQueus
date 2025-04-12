1678e963d8326cf59ad99d61149f1bba142ba35f
Pranav Kumar <pranav.jenus@gmail.com>
2025-04-12 10:08:56 +0530

    Update P16_Next_Greater_Element_2.cpp

--- P16_Next_Greater_Element_2.cpp
+++ P16_Next_Greater_Element_2.cpp
@@ -33,7 +33,7 @@ using namespace std;
                 We can do idx = i*n and obtain back from Arr
 
      Technique 3: Use Stack to store top() elements
-                 Read from last i.e. 2*n-1 if n=5 then i = 9
+                 Read from End i.e. 2*n-1 till 0 , if n=5 then i = 9 i.e 2*5-1
                  [2 10 12 1 11] [2 10 12  1 11]
                   0 1  2  3  4   5  6  7  8  9
 
@@ -55,6 +55,8 @@ void print_1D_v(vector<int> v)
     cout << endl;
 }
 
+// TC -> O(N^2)
+// SC -> O(N) for NGE Array
 void find_NGE_Sol1(vector<int> Arr,vector<int>& NGE)
 {
     int n = Arr.size();
@@ -84,6 +86,48 @@ void find_NGE_Sol1(vector<int> Arr,vector<int>& NGE)
 
 }
 
+// TC -> O(N^2)
+// SC -> O(N) for NGE Array
+void find_NGE_Sol2(vector<int> Arr,vector<int>& NGE)
+{
+    int n = Arr.size();
+    for(int i=0;i<n;i++)
+    {
+        NGE[i]=-1;
+        for(int j=i+1;j<=i+n-1;j++)
+        {
+            int idx = j%n;
+            if(Arr[idx]>Arr[i])
+            {
+                NGE[i]=Arr[idx];
+                break;
+            }
+        }
+    }
+}
+
+// TC -> O(2N)+O(2N) ~~ O(4N) or O(N)
+// TC -> O(N)+O(N) for stack and NGE Array
+void find_NGE_Sol3(vector<int> Arr,vector<int>& NGE)
+{
+    int n = Arr.size();
+    stack<int> St;
+    for(int i=2*n-1;i>=0;i--)
+    {
+        int idx = i%n;
+        while(!St.empty() && St.top()<=Arr[idx])
+        {
+            St.pop();
+        }
+
+        if(i<n)
+        {
+            NGE[i]=(!St.empty())?St.top():-1;
+        }
+        St.push(Arr[idx]);
+    }
+}
+
 int main()
 {
     cout << "Next Greater Element 2" << endl;
@@ -91,8 +135,22 @@ int main()
     int n = Arr.size();
     vector<int> NGE_1(n);
 
+    cout << "Arr" << endl;
+    print_1D_v(Arr);
+    cout << "Using Sol1" << endl;
     find_NGE_Sol1(Arr,NGE_1);
     print_1D_v(NGE_1);
+
+    cout << "Using Sol2" << endl;
+    vector<int> NGE_2(n);
+    find_NGE_Sol2(Arr,NGE_2);
+    print_1D_v(NGE_2);
+
+
+    cout << "Using Sol3" << endl;
+    vector<int> NGE_3(n);
+    find_NGE_Sol3(Arr,NGE_3);
+    print_1D_v(NGE_3);
 
     return 0;
 }
